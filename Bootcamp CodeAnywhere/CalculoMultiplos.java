54800ff8427c01a1e6c3818232e44078ba9914b3
Jean Carlos Albuquerque Souza <84343910+Jean-Carlos-A-Souza@users.noreply.github.com>
2021-05-26 14:45:44 -0300

    Create CalculoMultiplos.java

---
+++ Bootcamp CodeAnywhere/CalculoMultiplos.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Desafio{
+    
+    public static void main(String[] args) throws IOException {
+        Scanner leitor = new Scanner(System.in);
+        int valor1  = leitor.nextInt();
+        int valor2  = leitor.nextInt();
+
+        if ( valor1 % valor2 == 0 ) {
+            System.out.println("Sao Multiplos");
+        } else {
+             System.out.println("Nao sao Multiplos");
+        }
+    }
+    
+}
